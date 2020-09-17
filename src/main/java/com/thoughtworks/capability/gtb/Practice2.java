a001481b7e524faf30c2f95c02b77e48ce4801d1
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-18 00:52:44 +0800

    [fengyi_ling] feat:complete Practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
@@ -9,8 +10,24 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
+  public final static int dayValueOfThursday = 4;
+  public final static int dayValueOfSunDay = 7;
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+
+    int dayValue = date.getDayOfWeek().getValue();
+
+    if (isAfterThursday(dayValue)){
+      int hasNonWorkDaysBeforeMonday= dayValueOfSunDay - dayValue;
+      date = date.plusDays(hasNonWorkDaysBeforeMonday);
+    }
+    return date.plusDays(1);
+  }
+
+  static boolean isAfterThursday(int dayValue){
+    if (dayValue > dayValueOfThursday) {
+      return true;
+    }
+      return false;
   }
 }
