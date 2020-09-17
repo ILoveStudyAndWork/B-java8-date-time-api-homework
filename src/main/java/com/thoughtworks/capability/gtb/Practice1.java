38658eb05429f52340c9826c128506187782de15
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-18 00:12:58 +0800

    [fengyi_ling] feat:complete Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +13,18 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int thisYear = date.getYear();
+    LocalDate laborDayOfThisYear = LocalDate.of(thisYear,5,1);
+    long daysBetweenNextLaborDay;
+    if (date.isBefore(laborDayOfThisYear)) {
+      daysBetweenNextLaborDay = ChronoUnit.DAYS.between(date,laborDayOfThisYear);
+    } else {
+      int nextYear = thisYear +1;
+      LocalDate laborDayOfNextYear = laborDayOfThisYear.withYear(nextYear);
+      daysBetweenNextLaborDay = ChronoUnit.DAYS.between(date,laborDayOfNextYear);
+    }
+
+
+    return daysBetweenNextLaborDay;
   }
 }
